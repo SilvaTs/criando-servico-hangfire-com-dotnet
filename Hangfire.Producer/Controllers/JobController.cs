75647e9a3566ab11fd01a196ab3785be4de7050f
Daniel Silva <danielsilvatsi@outlook.com>
2024-07-17 14:08:00 -0300

    feat: add jobController producer

---
+++ Hangfire.Producer/Controllers/JobController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hangfire.Producer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class JobController
+{
+    private readonly IBackgroundJobClient _backgroundJobClient;
+
+    public JobController(IBackgroundJobClient backgroundJobClient)
+    {
+        _backgroundJobClient = backgroundJobClient;
+    }
+
+    [HttpPost]
+    public Task<string> FireAndForget([FromBody] string text)
+    {
+        _backgroundJobClient.Enqueue(() => Console.WriteLine(text));
+        return Task.FromResult(text);
+    }
+
+    [HttpPost]
+    [Route("delayed")]
+    public Task<string> Delayed([FromBody] string text)
+    {
+        _backgroundJobClient.Schedule(() => Console.WriteLine(text), TimeSpan.FromMinutes(1));
+        return Task.FromResult(text);
+    }
+
+    [HttpPost]
+    [Route("continuation")]
+    public Task<string> Continuation([FromBody] string text)
+    {
+        var jobId = _backgroundJobClient.Schedule(() =>
+            Console.WriteLine(text), TimeSpan.FromMinutes(1));
+
+        _backgroundJobClient.ContinueJobWith(jobId, () => Console.WriteLine(text));
+        return Task.FromResult(text);
+    }
+}
